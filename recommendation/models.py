553e1b57e072e8e028f0f3c0f3a8ebc49a03605e
shyyashowkat <shyxum96@gmail.com>
2021-06-04 19:31:38 +0530

    created movie model

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=64, null=True)
+    def __str__(self):
+        return self.title
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128, null=True)
+    poster = models.ImageField(upload_to='movie_poster')
+    genre = models.ManyToManyField(Genre)
+    def __str__(self):
+        return self.title
+
