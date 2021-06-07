2279b6576e02d54aa1c6c0e79b61d92f08f1524f
shyyashowkat <shyxum96@gmail.com>
2021-06-07 21:55:22 +0530

    login/logout

--- recommendation/models.py
+++ recommendation/models.py
@@ -1,17 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Genre(models.Model):
-    title = models.CharField(max_length=64, null=True)
-    def __str__(self):
-        return self.title
-
-
-class Movie(models.Model):
-    title = models.CharField(max_length=128, null=True)
-    poster = models.ImageField(upload_to='images/')
-    genre = models.ManyToManyField(Genre)
-    def __str__(self):
-        return self.title
-
--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
 from .views import *
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 urlpatterns = [
-    path('',home, name='home' ),
+    path('',  index, name='index'),
+	path('signup/', signup, name="signup"),
+	path('signin/', signin, name="signin"),  
+	path('signout/', signout, name="signout"),
 ]
-
-
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
